388ad3306563f3d1822a06f13f0ce4c9ff057a1a
hawon223 <hawon223@dsm.hs.kr>
2025-07-30 13:42:05 +0900

    csv
    csv파일로 저장함

--- emotion_ai.py
+++ emotion_ai.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
 import json
+import pandas as pd
 
 with open("data/sample_diary.json", "r", encoding="UTF-8")as f:
     diary_data = json.load(f)
@@ -9,5 +10,9 @@ pipe = pipeline("text-classification", model="hun3359/klue-bert-base-sentiment")
 for diary in diary_data:
     result = pipe(diary["text"])[0]
     diary["label"] = result["label"]
+    
+df = pd.DataFrame(diary_data)
+df.to_csv("data/sample.csv", index=False, encoding="utf-8 sig")
 
-print(diary_data)
+
+print("csv 파일 저장")
