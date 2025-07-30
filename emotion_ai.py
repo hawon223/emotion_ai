488efa8d525c92e13f4e594e566aabd527716215
hawon223 <hawon223@dsm.hs.kr>
2025-07-30 13:21:33 +0900

    json data connection
    json 파일을 불러와서 감정 분류함

--- emotion_ai.py
+++ emotion_ai.py
@@ -1,9 +1,13 @@
 from transformers import pipeline
+import json
+
+with open("data/sample_diary.json", "r", encoding="UTF-8")as f:
+    diary_data = json.load(f)
 
 pipe = pipeline("text-classification", model="hun3359/klue-bert-base-sentiment")
 
-text = "오늘은 기분이 너무 좋아"
+for diary in diary_data:
+    result = pipe(diary["text"])[0]
+    diary["label"] = result["label"]
 
-result = pipe(text)
-
-print(result)
+print(diary_data)
